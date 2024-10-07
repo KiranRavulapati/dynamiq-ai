01c34b338efc3007d107412e25b0b3c40e7950c4
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2024-10-07 16:27:31 +0300

    feat: test other tools with HumanLayer

--- examples/tools/use_function_tool_humanlayer.py
+++ examples/tools/use_function_tool_humanlayer.py
@@ -8,14 +8,18 @@ from examples.llm_setup import setup_llm
 from dynamiq import Workflow
 from dynamiq.callbacks import TracingCallbackHandler
 from dynamiq.flows import Flow
-
+from dynamiq.nodes.agents.react import ReActAgent
+from dynamiq.connections import E2B as E2BConnection
+from dynamiq.nodes.tools.e2b_sandbox import E2BInterpreterTool
+from dynamiq.runnables import RunnableConfig
 hl = HumanLayer()
 
 AGENT_ROLE = "Professional mathematician"
 AGENT_GOAL = "Answer on questions"
-INPUT_QUESTION = "What is (5 + 10) * 3?"
+INPUT_QUESTION = "What is (5 + 10) * 3."
+INPUT_QUESTION1 = "print('test') to console"
 
-def run_simple_workflow() -> tuple[str, dict]:
+def run_simple_workflow(test_type = 1) -> tuple[str, dict]:
     """
     Execute a workflow using the OpenAI agent to process a predefined question. Uses HumanLayer.
 
@@ -26,6 +30,17 @@ def run_simple_workflow() -> tuple[str, dict]:
         Exception: Captures and prints any errors during workflow execution.
     """
     llm = setup_llm()
+
+    e2b_tool = E2BInterpreterTool(
+        connection=E2BConnection()
+    )
+    @function_tool
+    @hl.require_approval() # Add HumanLayer support
+    def python_tool(code: str):
+        f"""{e2b_tool.description}"""
+        return e2b_tool.execute({'python': code})
+    
+    tool_python = python_tool() 
 
     class AddNumbersTool(FunctionTool[int]):
         name: str = "Add Numbers Tool"
@@ -45,20 +60,27 @@ def run_simple_workflow() -> tuple[str, dict]:
     multiply_tool = multiply_numbers()
     add_tool = AddNumbersTool()
 
+    if test_type == 0:
+        tools=[add_tool, multiply_tool]
+        input_question = INPUT_QUESTION
+    elif test_type == 1:
+        tools=[tool_python]
+        input_question = INPUT_QUESTION1
+
     agent = ReActAgent(
         name=" Agent",
         llm=llm,
         role=AGENT_ROLE,
         goal=AGENT_GOAL,
         id="agent",
-        tools=[add_tool, multiply_tool]
+        tools=tools
     )
     tracing = TracingCallbackHandler()
     wf = Workflow(flow=Flow(nodes=[agent]))
 
     try:
         result = wf.run(
-            input_data={"input": INPUT_QUESTION},
+            input_data={"input": input_question},
             config=RunnableConfig(callbacks=[tracing]),
         )
         json.dumps(
